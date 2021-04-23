f7f0c1f0488b6e18b9f07476bd58bcc886dc030d
lprocel1 <81196226+lprocel1@users.noreply.github.com>
2021-04-22 22:57:41 -0400

    Development files (#2)
    * Create Player
    
    * Create twoPlayerShell
    
    * Create playerUnitTest
    
    * Update twoPlayerShell
    
    * Create twoPlayerGame
    
    * Update twoPlayerGame
    
    * Create StartUpWindow
    
    * Add files via upload
    
    * Delete Player
    
    * Delete StartUpWindow
    
    * Delete twoPlayerGame
    
    * Delete twoPlayerShell
    
    * Delete playerUnitTest

---
+++ Player.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 22 21:59:04 2021
+
+@author: proce
+"""
+
+MARKER = ['+','o', 'x']
+
+class CurrentPlayer:
+    def __init__(self, player1, player2):
+        self.setMarker(player1,player2)
+        self.setName(player1, player2)
+        self.playerStorage=[player1,player2]
+    
+    def setMarker(self, player1, player2):
+        if player1.playing:
+            self.marker=player1.marker
+        else: self.marker=player2.marker
+        
+    def setName(self, player1, player2):
+        if player1.playing:
+            self.name=player1.name
+        else: self.name=player2.name
+    
+    def switchPlayers(self):
+        player1 = self.playerStorage[0] 
+        player2 = self.playerStorage[1]
+        if player1.playing == True:
+            player1.setPlayStatus(False)
+            player2.setPlayStatus(True)
+        else:
+            player2.setPlayStatus(False)
+            player1.setPlayStatus(True)
+        self.setMarker(player1, player2)
+        self.setName(player1, player2)
+    
+    def gameOver(self):
+        if self.playerStorage[0].win or self.playerStorage[1].win:
+            return True
+        
+    def setWinner(self):
+        for user in self.playerStorage:
+            if user.marker == self.marker:
+                user.winner(True)
+    
+    def getPlayerName(self,playerNum):
+        return self.playerStorage[playerNum-1].name
+
+class Player:
+    def __init__(self, playerVal, playerName, marker=None):
+        self.player = playerVal
+        self.name = playerName
+        self.setMarker(playerVal,marker)
+        self.setPlayStatus(playBool = False)
+        self.winner(False)
+        
+    def __call__(self):
+        if self.player == 0:
+            return [str(self.name), 'Player #2', 'Marker = "' + str(self.marker) + '"']
+        else:
+            return [str(self.name),'Player #' + str(self.player), 'Marker = "' + str(self.marker) + '"']
+    
+    def setPlayStatus(self, playBool):
+        self.playing = playBool
+        
+    def setMarker(self, playerVal, marker):
+        if marker is not None:
+            self.marker = marker
+        else:
+            self.marker = MARKER[playerVal]
+            
+    def winner(self, winBool):
+        self.win = winBool
